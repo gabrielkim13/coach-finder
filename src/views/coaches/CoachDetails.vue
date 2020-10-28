024aed4725569d60abf48f86881d0df22f7efbaf
Gabriel Kim <gabrielkim13@gmail.com>
2020-10-28 19:57:53 -0300

    Fix CoachDetails page

--- src/views/coaches/CoachDetails.vue
+++ src/views/coaches/CoachDetails.vue
@@ -1,13 +1,68 @@
 <template>
-  <h1>CoachDetails</h1>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
-  <router-view />
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+
+  <section>
+    <base-card>
+      <h2>Interested? Reach out now!</h2>
+      <base-button link :to="contactLink">Contact</base-button>
+    </base-card>
+    <router-view />
+  </section>
+
+  <base-card>
+    <base-badge v-for="area in areas" :key="area" :title="area" :type="area" />
+    <p>{{ description }}</p>
+  </base-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import BaseBadge from "@/components/ui/BaseBadge.vue";
+import BaseButton from "@/components/ui/BaseButton.vue";
+import BaseCard from "@/components/ui/BaseCard.vue";
+
+import { Coach } from "@/store/modules/coaches";
+
 export default defineComponent({
-  name: "CoachDetails"
+  name: "CoachDetail",
+
+  components: {
+    BaseBadge,
+    BaseButton,
+    BaseCard
+  },
+
+  props: ["id"],
+
+  data: () => ({
+    coach: {} as Coach | undefined
+  }),
+
+  computed: {
+    fullName(): string {
+      return `${this.coach?.firstName} ${this.coach?.lastName}`;
+    },
+    contactLink(): string {
+      return `coaches/${this.id}/contact`;
+    },
+    areas(): string[] {
+      return this.coach?.areas || [];
+    },
+    rate(): number {
+      return this.coach?.hourlyRate || 0;
+    }
+  },
+
+  created() {
+    const coaches = this.$store.getters["coaches/coaches"] as Coach[];
+
+    this.coach = coaches.find(findCoach => findCoach.id === this.id);
+  }
 });
 </script>
