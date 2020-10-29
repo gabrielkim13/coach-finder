b5b83177f7b8d704a025c8e816e28da6b76d9fd1
Gabriel Kim <gabrielkim13@gmail.com>
2020-10-29 14:42:25 -0300

    Add not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>NotFound</h1>
+  <base-card>
+    <h2>Page not found</h2>
+    <p>
+      This page could not be found - maybe check out our
+      <router-link to="/coaches">coaches</router-link>.
+    </p>
+  </base-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import BaseCard from "@/components/ui/BaseCard.vue";
+
 export default defineComponent({
-  name: "NotFound"
+  name: "NotFound",
+
+  components: {
+    BaseCard
+  }
 });
 </script>
