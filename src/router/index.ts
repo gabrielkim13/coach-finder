00a4d999bdad6b84cf9baf5d17ab9c2e74aeb8f3
Gabriel Kim <gabrielkim13@gmail.com>
2020-10-29 20:16:15 -0300

    Add route guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,8 @@ import RequestsList from "@/views/requests/RequestsList.vue";
 import UserAuth from "@/views/UserAuth.vue";
 import NotFound from "@/views/NotFound.vue";
 
+import store from "@/store";
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
@@ -34,17 +36,20 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/register",
     name: "CoachRegister",
-    component: CoachRegister
+    component: CoachRegister,
+    meta: { requiresAuth: true }
   },
   {
     path: "/requests",
     name: "RequestsList",
-    component: RequestsList
+    component: RequestsList,
+    meta: { requiresAuth: true }
   },
   {
     path: "/auth",
     name: "UserAuth",
-    component: UserAuth
+    component: UserAuth,
+    meta: { requiresNotAuth: true }
   },
   {
     path: "/:pathMatch(.*)*",
@@ -56,6 +61,16 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.getters["auth/isAuthenticated"])
+    return next("/auth");
+
+  if (to.meta.requiresNotAuth && store.getters["auth/isAuthenticated"])
+    return next("/coaches");
+
+  return next();
 });
 
 export default router;
